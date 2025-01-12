621d4205d801bee2842f40bba45d94501d8e8c62
habeeb-developer <habeebdeveloper@gmail.com>
2025-01-12 16:06:27 +0800

    zellLiew-learnJavaScript Course - first exercise completed

---
+++ app.js
@@ -0,0 +1,34 @@
+// alert('Learn JavaScript');
+
+
+
+// 1. Strings
+
+console.log('String represents only text habeeb');
+
+// handling strings with apostrophes
+console.log('habeeb\'s   birthday');
+
+//handling strings with backticks
+console.log(`habeeb's   birthday`)
+
+//adding strings together with concatenation
+console.log('habeeb ' + 'mohamed');
+
+
+
+// 2. Numbers
+
+// adding numbers together
+console.log(20+2);
+console.log(20-2);
+console.log(20/2);
+console.log(20*2);
+
+//find the remaider of 22/5;
+console.log(22%5);
+
+// 3. Booleans
+
+console.log(true);
+console.log(false);
---
+++ jsBasics_exOne.js
@@ -0,0 +1,48 @@
+// JS Basics - Exercise
+alert('javaScript exercise one - check console for results');
+
+// 1.Create a String and console.log it.
+// 2.Add two strings together.
+// 3.Create a Number and console.log it.
+// 4.Add 27 and 73.
+// 5.Subtract 30 from 50.
+// 6.Multiply 5 and 10.
+// 7.Divide 100 by 10.
+// 8.Get the remainder of 500 divided by 3.
+// 9.Create a Boolean and console.log it.
+
+
+// 1.Create a String and console.log it.
+console.log('Result: '+ 'This is a string, it represents text data only');
+
+
+// 2.Add two strings together.
+console.log('Two Strings Concatenate 1 & 2: '+ 'This is string 1 ' + 'This is string 2');
+
+
+// 3.Create a Number and console.log it.
+console.log('Numbers: ' + 123456789);
+
+// 4.Add 27 and 73.
+console.log(27);
+console.log(27 + 73);
+
+
+// 5.Subtract 30 from 50.
+console.log(50-30);
+
+
+// 6.Multiply 5 and 10.
+console.log(5*10);
+
+
+// 7.Divide 100 by 10.
+console.log(100/10);
+
+
+// 8.Get the remainder of 500 divided by 3.
+console.log(500%3);
+
+// 9.Create a Boolean and console.log it.
+console.log(true);
+console.log(false);
